9c1a8cf7ee7f0a4415d74c605a4359015745c650
beardness-andy-dev <beardness.andy.dev@gmail.com>
2025-11-01 20:03:04 +0400

    refactor tests

--- test/presentation/utils/date_formatter_test.dart
+++ test/presentation/utils/date_formatter_test.dart
@@ -3,22 +3,28 @@ import 'package:tcp_penguin/presentation/utils/date_formatter.dart';
 
 void main() {
   group('DateFormatter', () {
-    test('форматирует UTC дату детерминированно', () {
+    test('Format my birthday', () {
       final dt = DateTime.utc(2025, 5, 8, 18, 50);
       final formatted = DateFormatter.format(dt);
       expect(formatted, '2025-05-08 18:50');
     });
 
-    test('ведущие нули для месяца/дня/часа/минут', () {
+    test('Leading zeros for month/day/hour/minute', () {
       final dt = DateTime.utc(2025, 2, 3, 4, 6);
       final formatted = DateFormatter.format(dt);
       expect(formatted, '2025-02-03 04:06');
     });
 
-    test('конец года', () {
+    test('End of the year', () {
       final dt = DateTime.utc(2025, 12, 31, 23, 59);
       final formatted = DateFormatter.format(dt);
       expect(formatted, '2025-12-31 23:59');
+    });
+
+    test('Beginning of the year', () {
+      final dt = DateTime.utc(2025, 1, 1, 0, 0);
+      final formatted = DateFormatter.format(dt);
+      expect(formatted, '2025-01-01 00:00');
     });
   });
 }
