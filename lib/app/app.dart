47921cead93bb03d974faab7739d32bc69931390
beardness-andy-dev <beardness.andy.dev@gmail.com>
2025-11-01 19:31:29 +0400

    add dynamic colors

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:tcp_penguin/app/navigation/router.dart';
 
@@ -6,13 +7,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'TCP Penguin',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      routerConfig: goRouter,
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        final lightColorScheme =
+            lightDynamic ??
+            ColorScheme.fromSeed(
+              seedColor: Colors.pink,
+              brightness: Brightness.light,
+            );
+
+        final darkColorScheme =
+            darkDynamic ??
+            ColorScheme.fromSeed(
+              seedColor: Colors.pink,
+              brightness: Brightness.dark,
+            );
+
+        return MaterialApp.router(
+          title: 'TCP Penguin',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(colorScheme: lightColorScheme, useMaterial3: true),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.system,
+          routerConfig: goRouter,
+        );
+      },
     );
   }
 }
