c6d19fa83b834aff57f94f63c0ddb83dcb95264d
beardness-andy-dev <beardness.andy.dev@gmail.com>
2025-11-02 16:56:33 +0400

    add mixed result at cuncurency tcp scanner tests

--- test/presentation/domain/concurency_tcp_scanner/concurency_tcp_scanner_test.dart
+++ test/presentation/domain/concurency_tcp_scanner/concurency_tcp_scanner_test.dart
@@ -14,7 +14,7 @@ void main() {
     concurencyTcpScanner = ConcurencyTcpScanner(tcpScanner: tcpScanner);
   });
 
-  test('Default scan', () async {
+  test('Scan with success results', () async {
     final String host = 'example.com';
     final Duration timeout = Duration(milliseconds: 100);
 
@@ -28,6 +28,49 @@ void main() {
       ),
     ).thenAnswer((_) async {
       return TcpScanerResultSuccess();
+    });
+
+    await concurencyTcpScanner.runConcurrently(
+      host: host,
+      portStart: 1,
+      portEnd: 10,
+      maxConcurrent: 100,
+      timeoutMs: 100,
+      onProgress: (double p) {
+        progress = p;
+      },
+    );
+
+    expect(progress, 1);
+
+    verify(
+      () => tcpScanner.scan(
+        host: any(named: 'host'),
+        port: any(named: 'port'),
+        timeout: timeout,
+      ),
+    ).called(10);
+  });
+
+  test('Scan with mixed results', () async {
+    final String host = 'example.com';
+    final Duration timeout = Duration(milliseconds: 100);
+
+    double progress = 0;
+
+    when(
+      () => tcpScanner.scan(
+        host: host,
+        port: any(named: 'port'),
+        timeout: timeout,
+      ),
+    ).thenAnswer((invocation) async {
+      final int port = invocation.namedArguments[#port] as int;
+      if (port % 2 == 0) {
+        return TcpScanerResultSuccess();
+      } else {
+        return TcpScanerResultFailure();
+      }
     });
 
     await concurencyTcpScanner.runConcurrently(
